58ab56a95a3a9452326275e98dbaa02123bf1aac
Timm Jensen <4342842@rts-365.dk>
2018-06-08 11:16:44 +0200

    Sat arrangementer til henting

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@ module.exports = function(app) {
 
     const findTitel = require('../services/titel');
     const findMenuer = require('../services/menu');
+	const events = require('../services/events');
 
     /* Indlæser forsiden */
     app.get('/', async (req, res, next) => {
         try {
             titel = await findTitel.getOne();
-            menu = await findMenuer.getAll();
+			menu = await findMenuer.getAll();
+
+			arrangementer = await events.getAll();
+			
             res.render('pages/index', {
                 titel: titel.titel,
-                menu: menu
+				menu: menu,
+				arrangementer: arrangementer
             });
         } catch (err) {
             console.log(err);
